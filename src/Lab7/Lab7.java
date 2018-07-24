784a210081e3b627598a11efabaef9929008a8c7
Kari Mcfarlin <40720949+morri196@users.noreply.github.com>
2018-07-24 08:58:49 -0400

    Update Lab7.java

--- src/Lab7/Lab7.java
+++ src/Lab7/Lab7.java
@@ -8,7 +8,7 @@ public class Lab7 {
 		
 		
 		String name = Lab7Validator.getStringMatchingRegex(scnr, "Enter first name: ", "[A-Z][a-z]{1,29}");
-		String email = Lab7Validator.getStringMatchingRegex(scnr, "Enter a valid email: (ex. morri192@gmail.com)" , "[A-Za-z]{5,30}]@[A-Za-z]{5,10}.[A-Za-z0-9]{2,3}");
+		String email = Lab7Validator.getStringMatchingRegex(scnr, "Enter a valid email: (ex. morri192@gmail.com)" , "[A-Za-z0-9]{5,30}]@[A-Za-z0-9]{5,10}.[A-Za-z0-9]{2,3}");
 		String phoneNum = Lab7Validator.getStringMatchingRegex(scnr, "Enter a valid phone number: ", "^\\d{3}-\\d{3}-\\d{4}$");
 		String date = Lab7Validator.getStringMatchingRegex(scnr, "Enter a Date: (ex. dd-mm-yyyy)", "\\d{2}-\\d{2}-\\d{4}");
 		
